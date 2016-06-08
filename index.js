49c74b65ffbd39c79a58b2610ce0871990abee62
Espen Klem <espen.klem@gmail.com>
2016-06-08 14:19:31 +0200

    Just the layout

---
+++ index.js
@@ -0,0 +1,4 @@
+// 1: Takes a search-index query object as input
+// 2: Takes a synonyms.json file as input
+// 3: Checks if any words in the AND-part equals any key in the synonyms.json
+// 4: If so: Adds a new AND-part to the array with replacement word and returns the query object
